dcc56b296473f643e364b5c1b3b8c734edef416c
AlexSp <36576348+AlexSperka@users.noreply.github.com>
2020-05-21 08:51:06 +0200

    SortOrders Init

--- Production/createOrders/index.js
+++ Production/createOrders/index.js
@@ -1,17 +1,8 @@
 /**
  * Function to add new Orders to production table
  * 
- * @alias    realName
+ * @alias    esi_prod_createOrders
  * @memberof ProductionTeamESI
- *
- * @see  Function/class relied on
- * @link URL
- * @global
- *
- * @fires   eventName
- * @fires   className#eventName
- * @listens event:eventName
- * @listens className~event:eventName
  *
  * @param {Object} orderObject     JSON Object with new Order
  * {
@@ -30,7 +21,7 @@
  * @return {String} Return value description.
  */
 
-
+/********************************* Librarys ***********************************/
 const mysql = require('mysql2'); // require mysql
 var moment = require("moment-timezone");
 
@@ -61,26 +52,27 @@ if (typeof client === 'undefined') {
     database: process.env.RDS_DATABASE,
   });
 
-  client.connect()
+  client.connect();
 }
 /******************************************************************************/
 /********************************* Export Handler *****************************/
 exports.handler = (event, context, callback) => {
 
   // var date = moment();
-  time = "\'" + moment().format('HH:mm:ss') + "\'"  //get UTC Time
-  date = "\'" + moment().format('DD.MM.YYYY') + "\'"
+  time = "\'" + moment().format('HH:mm:ss') + "\'";  //get UTC Time
+  date = "\'" + moment().format('DD.MM.YYYY') + "\'";
   console.log("date & time UTC: " + time + " and " + date);
 
   console.log('Received event:', JSON.stringify(event, null, 2));
-  let newOrder = JSON.stringify(event)
-  newOrder = JSON.parse(newOrder)
+  let newOrder = JSON.stringify(event);
+  newOrder = JSON.parse(newOrder);
   // console.log('Color Name: ', newOrder.body.colorName);
 
   context.callbackWaitsForEmptyEventLoop = false;
 
   callDB(client, writeOrdersToDB(newOrder, date, time), callback);
-}
+  // asynchCallDB(client, writeOrdersToDB(newOrder, date, time));
+};
 
 /********************************* Database Call ******************************/
 const callDB = (client, queryMessage, callback) => {
@@ -94,7 +86,39 @@ const callDB = (client, queryMessage, callback) => {
         })
       });
     });
-}
+};
+
+/********************************* Asynch Database Call ***********************/
+
+/*  Timeout right now, not working   */
+const asynchCallDB = async (connection, queryMessage) => {
+  try {
+      const data = await new Promise((resolve, reject) => {
+          connection.connect(function (err) {
+              if (err) {
+                  reject(err);
+              }
+              connection.query(queryMessage,
+                  function (err, result) {
+                      if (err) {
+                          console.log("Error->" + err);
+                          reject(err);
+                      }
+                      resolve(result);
+                  });
+          });
+      });
+      return {
+          statusCode: 200,
+          body: JSON.stringify(data)
+      };
+  } catch (err) {
+      return {
+          statusCode: 400,
+          body: err.message
+      };
+  }
+};
 
 /********************************* Creating Order String for SQL***************/
 const writeOrdersToDB = function (newOrder, date, time) {
@@ -113,22 +137,22 @@ const writeOrdersToDB = function (newOrder, date, time) {
   let newOrderString = "INSERT INTO testdb.ProdTable values(" + date + "," + time + "," + endDate + "," + productionOrderNumber + "," + articleNumber + "," + color + "," + colorName + "," + quantity + "," + hasPrint + "," + motiveNumber + "," + prodSortNum + "," + prodStatus + "," + splitOrders + ")";
   console.log(newOrderString);
   return (newOrderString);
-}
+};
 
 /********************************* Helper Function CREATE TABLE ***************/
 const createTableSQL = function () {
   var queryMessage = 'CREATE TABLE ProdTable (date DATE, time TIME, endDate DATE, prodOrderNum VARCHAR(255), articleNumber INT, colorHEX VARCHAR(255), colorName VARCHAR(255), quantity INT, hasPrint TINYINT(1), motiveNumber INT, ProdSortNum INT, c TINYINT, m TINYINT, y TINYINT, k TINYINT, prodStatus VARCHAR(30), splitOrders VARCHAR(255) )';
   return (queryMessage);
-}
+};
 
 /********************************* Helper Function CREATE DATABASE*************/
 const createDatabaseSQL = function () {
   var queryMessage = 'CREATE DATABASE testdb';
   return (queryMessage);
-}
+};
 
 /********************************* Helper Function GET STUFF FROM DB***********/
 const getOrdersFromDB = function () {
-  var queryMessage = 'SELECT * FROM testdb.ProductionDB LIMIT 10';
+  var queryMessage = 'SELECT * FROM testdb.ProdTable LIMIT 10';
   return (queryMessage);
-}
+};
--- Production/sortOrders/index.js
+++ Production/sortOrders/index.js
@@ -1,147 +1,84 @@
-/******** Code from https://dzone.com/articles/aws-lambda-with-mysql-rds-and-api-gateway ******/
+/**
+ * Function to add new Orders to production table
+ * 
+ * @alias    esi_prod_sortOrders
+ * @memberof ProductionTeamESI
+ *
+ * @fires   esi_prod_callCSV
+ * @fires   className#eventName
+ * @listens event:eventName
+ * @listens className~event:eventName
+ *
+ * @param none
+ *
+ * @return {String} Return value description.
+ */
 
-/******* Create DB using lambda **************************************************************/
+/********************************* Librarys ***********************************/
+const mysql = require('mysql2'); // require mysql
+var moment = require("moment-timezone");
 
-const mysql = require('mysql');
-const connection = mysql.createConnection({
-    //following param coming from aws lambda env variable 
-    host: process.env.RDS_LAMBDA_HOSTNAME,
-    user: process.env.RDS_LAMBDA_USERNAME,
-    password: process.env.RDS_LAMBDA_PASSWORD,
-    port: process.env.RDS_LAMBDA_PORT,
-    // calling direct inside code  
-    connectionLimit: 10,
-    multipleStatements: true,
-    // Prevent nested sql statements 
-    connectionLimit: 1000,
-    connectTimeout: 60 * 60 * 1000,
-    acquireTimeout: 60 * 60 * 1000,
-    timeout: 60 * 60 * 1000,
-    debug: true
-});
+/********************************* Variables **********************************/
+var date = 0;
+var time = 0;
 
-exports.handler = async (event) => {
-    try {
-        const data = await new Promise((resolve, reject) => {
-            connection.connect(function (err) {
-                if (err) {
-                    reject(err);
-                }
-                connection.query('CREATE DATABASE testdb', function (err, result) {
-                    if (err) {
-                        console.log("Error->" + err);
-                        reject(err);
-                    }
-                    resolve(result);
-                });
-            })
+/********************************* SQL Connection *****************************/
+// If 'client' variable doesn't exist
+if (typeof client === 'undefined') {
+    // Connect to the MySQL database
+    var client = mysql.createConnection({
+      host: process.env.RDS_LAMBDA_HOSTNAME,
+      user: process.env.RDS_LAMBDA_USERNAME,
+      password: process.env.RDS_LAMBDA_PASSWORD,
+      port: process.env.RDS_LAMBDA_PORT,
+      database: process.env.RDS_DATABASE,
+    });
+  
+    client.connect();
+  }
+
+/******************************************************************************/
+/********************************* Export Handler *****************************/
+exports.handler = (event, context, callback) => {
+
+    // var date = moment();
+    time = "\'" + moment().format('HH:mm:ss') + "\'";  //get UTC Time
+    date = "\'" + moment().format('DD.MM.YYYY') + "\'";
+    console.log("date & time UTC: " + time + " and " + date);
+  
+    console.log('Received event:', JSON.stringify(event, null, 2));
+    let newOrder = JSON.stringify(event);
+    newOrder = JSON.parse(newOrder);
+    // console.log('Color Name: ', newOrder.body.colorName);
+  
+    context.callbackWaitsForEmptyEventLoop = false;
+  
+    callDB(client, selectOrdersFromDB);
+    // asynchCallDB(client, writeOrdersToDB(newOrder, date, time));
+  };
+
+/********************************* Database Call ******************************/
+const callDB = (client, queryMessage, callback) => {
+    client.query(queryMessage,
+      function (error, results) {
+        callback(null, {
+          statusCode: 200,
+          body: JSON.stringify({
+            data: 'Die Aufträge wurden erfolgreich sortiert!',
+            dataReturn: results
+          })
         });
-        return {
-            statusCode: 200,
-            body: JSON.stringify(data)
-        }
-    } catch (err) {
-        return {
-            statusCode: 400,
-            body: err.message
-        }
-    }
-};
+      });
+  };
 
-/******* Insert table in DB using lambda **************************************************************/
+/********************************* Helper Function GET STUFF FROM DB***********/
+const getOrdersFromDB = function () {
+    var queryMessage = 'SELECT * FROM testdb.ProdTable LIMIT 10';
+    return (queryMessage);
+  };
 
-exports.handler = async (event) => {
-    const sql = "CREATE TABLE MESSAGE (message VARCHAR(255))";
-    connection.query(sql, function (err, result) {
-        if (err) throw err;
-        console.log("Table created");
-    });
-    return "Table Created"
-};
-const mysql = require('mysql');
-const connection = mysql.createConnection({
-//following param coming from aws lambda env variable  
-    host: process.env.RDS_LAMBDA_HOSTNAME,
-    user: process.env.RDS_LAMBDA_USERNAME,
-    password: process.env.RDS_LAMBDA_PASSWORD,
-    port: process.env.RDS_LAMBDA_PORT,
-    database: process.env.RDS_DATABASE,
-// calling direct inside code
-    connectionLimit: 10,
-    multipleStatements: true,
-// Prevent nested sql statements   
-    connectionLimit: 1000,
-    connectTimeout: 60 * 60 * 1000,
-    acquireTimeout: 60 * 60 * 1000,
-    timeout: 60 * 60 * 1000,
-    debug: true
-});
-exports.handler = async (event) => {
-    try {
-        const data = await new Promise((resolve, reject) => {
-            connection.connect(function (err) {
-                if (err) {
-                    reject(err);
-                }
-                connection.query('CREATE TABLE ProductionDB (date DATE, time TIME, orderNumber VARCHAR(255), customerLastName VARCHAR(255), customerFirstName VARCHAR(255), street VARCHAR(255), postCode INT, city VARCHAR(255), country VARCHAR(255), articleNumber INT, color VARCHAR(255), quantity INT, hasPrint TINYINT(1), motiveNumber INT )',
-                    function (err, result) {
-                        if (err) {
-                            console.log("Error->" + err);
-                            reject(err);
-                        }
-                        resolve(result);
-                    });
-            })
-        });
-        return {
-            statusCode: 200,
-            body: JSON.stringify(data)
-        }
-    } catch (err) {
-        return {
-            statusCode: 400,
-            body: err.message
-        }
-    }
-};
-
-
-/******* Insert records using lambda **************************************************************/
-/*  date DATE, time TIME, orderNumber VARCHAR(255), customerLastName VARCHAR(255), customerFirstName VARCHAR(255), street VARCHAR(255), postCode INT, city VARCHAR(255), country VARCHAR(255), articleNumber INT, color VARCHAR(255), quantity INT, hasPrint TINYINT(1), motiveNumber INT    */
-exports.handler = (event, context, callback) => {
-// allows for using callbacks as finish/error-handlers
-
-    context.callbackWaitsForEmptyEventLoop = false;
-    const sql = "INSERT INTO testdb.ProductionDB values('2017/03/27','09:12:13','C-20170327-90123','Müller','Johannes','Langestraße 5',77723,'Gengenbach','Deutschland',10000001,'#009900',2,'Y',3456)";
-    connection.query(sql, function (err, result) {
-        if (err) throw err;
-        console.log("1 record inserted");
-    });
-};
-
-
-/******* Select records using lambda **************************************************************/
-
-exports.handler = (event, context, callback) => {
-    // allows for using callbacks as finish/error-handlers
-    context.callbackWaitsForEmptyEventLoop = false;
-    const sql = "select * from testdb.Employee ";
-    con.query(sql, function (err, result) {
-        if (err) throw err;
-        callback(null, result)
-    });
-};
-
-/******* Select records with criteria using lambda **************************************************************/
-
-exports.handler = (event, context, callback) => {
-// allows for using callbacks as finish/error-handlers
-    context.callbackWaitsForEmptyEventLoop = false;
-    const sql = "select * from  testdb.Employee where emp_id = " + event.emp_id;
-    con.query(sql, function (err, result) {
-        if (err) throw err;
-        callback(null, result)
-    });
-};
-
-
+/********************************* Helper Function GET STUFF FROM DB***********/
+const selectOrdersFromDB = function () {
+    var queryMessage = "SELECT * FROM  testdb.ProdTable WHERE prodStatus = " + 'open';
+    return (queryMessage);
+  };
